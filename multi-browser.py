9a00fcf8b62c9e560ecc18d520af0e9239260c60
Emmanuel Law <Emmanuel.law@gmail.com>
2017-07-13 14:06:28 +1200

    Add Menu context to enable and disable highlighting

--- multi-browser.py
+++ multi-browser.py
@@ -2,8 +2,13 @@ from burp import IBurpExtender
 from burp import IHttpListener	
 from burp import IProxyListener
 from burp import IInterceptedProxyMessage
+from burp import IContextMenuFactory
+
+
+from javax.swing import JMenuItem
+from java.awt.event import ActionListener
 from java.io import PrintWriter
-class BurpExtender(IBurpExtender,IProxyListener):
+class BurpExtender(IBurpExtender,IProxyListener, IContextMenuFactory,ActionListener):
 	def registerExtenderCallbacks( self, callbacks):
 		# keep a reference to our callbacks and helper object
 		self._callbacks=callbacks
@@ -14,16 +19,20 @@ class BurpExtender(IBurpExtender,IProxyListener):
 		# Keep Track of Browsers
 		self._browser={}
 		# Colors for different browsers
-		self.colors=["red", "pink", "green","magenta","cyan", "gray", "yellow",None]
+		self.colors=["red", "pink", "green","magenta","cyan", "gray", "yellow"]
  
 		
-		self._callbacks.setExtensionName("Multi-Browser")
+		self._callbacks.setExtensionName("Multi-Browser Highlighting")
+		self.isEnabled=False
 		
 		#IExtensionHelpers helpers = callbacks.getHelpers()
 		callbacks.registerProxyListener(self)
+		callbacks.registerContextMenuFactory(self)
 		return
 
 	def processProxyMessage(self, messageIsRequest, message):
+		if self.isEnabled == False:
+			return
 		#	self._stdout.println(("Proxy request to " if messageIsRequest else "Proxy response from ") + message.getMessageInfo().getHttpService().toString())
 		if messageIsRequest == False:
 			return
@@ -40,3 +49,21 @@ class BurpExtender(IBurpExtender,IProxyListener):
 
 		self.stdout.println(self._browser[browser_agent]["agent"])
 		message.getMessageInfo().setHighlight(self._browser[browser_agent]["color"])
+
+	def createMenuItems(self, invocation):
+		if invocation.getInvocationContext() == invocation.CONTEXT_PROXY_HISTORY:
+			mymenu=[]
+			if self.isEnabled:
+				item=JMenuItem("Multi-Browser Highlight (Running): Click to Disable ")
+			else:
+				item=JMenuItem("Multi-Browser Highlight (Stopped): Click to Enable ")
+			item.addActionListener(self)
+			mymenu.append(item)
+			return mymenu
+
+		else:
+			return None
+	def actionPerformed(self, actionEvent):
+		self.isEnabled= not self.isEnabled
+		
+
